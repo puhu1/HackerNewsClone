864d5551814dd85e8f111bee035021d9190db865
goArina <arina@xoxoday.com>
2019-08-19 20:56:09 +0530

    Adding App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import TopNews from './home/topNews';
+import Comments from './home/comments'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <header className="App-header"></header> */}
+          <Switch>
+          <Route path="/comment/:id" component={Comments}/>
+          <Route path="/" component={TopNews}/>
+          
+          </Switch>
+          
+        
+      </div>
+    </BrowserRouter>
+
   );
 }
 
